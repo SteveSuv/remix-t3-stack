37126f64fb315b8e25c50307349b41b4ba3bfcfe
zhangtaojun <zhangtaojun@leetcode.cn>
2024-08-02 14:15:08 +0800

    feat: update lazy route

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         unstable_singleFetch: true,
-        unstable_fogOfWar: true,
+        unstable_lazyRouteDiscovery: true,
       },
     }),
     tsconfigPaths(),
