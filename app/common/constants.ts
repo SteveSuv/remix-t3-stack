223eb54a9b5a5f84322eec7f1889f7f2544b089e
zhangtaojun <zhangtaojun@leetcode.cn>
2025-01-02 14:20:31 +0800

    feat: upgrade packages

--- app/common/constants.ts
+++ app/common/constants.ts
@@ -3,5 +3,8 @@ export const JWT_KEY = "ACCESS_TOKEN";
 export const COOKIE_MAX_AGE = 60 * 60 * 24 * 30; // 30 days
 
 const DEV_TRPC_URL = "http://localhost:3000/trpc";
-const PROD_TRPC_URL = "http://localhost:3000/trpc"; // change to your real domain when deploy to prod
+
+// change to your real domain when deploy to prod
+const PROD_TRPC_URL = "http://localhost:3000/trpc";
+
 export const TRPC_URL = IS_PROD ? PROD_TRPC_URL : DEV_TRPC_URL;
