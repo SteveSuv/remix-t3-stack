7a5b385e52621e0e9ff8a96405807671aad2f679
zhangtaojun <2507698457@qq.com>
2025-03-23 15:26:00 +0800

    feat: upgrade trpc to v11

--- app/common/trpc.ts
+++ app/common/trpc.ts
@@ -1,15 +1,15 @@
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import SuperJSON from "superjson";
 import { AppRouter } from "~/.server/router";
 import { TRPC_URL } from "./constants";
 
 // use trpcServer to fetch in server environment, like in loader, for passing cookies to trpc endpoint
 export const trpcServer = (request?: Request) => {
-  return createTRPCProxyClient<AppRouter>({
-    transformer: SuperJSON,
+  return createTRPCClient<AppRouter>({
     links: [
       httpBatchLink({
         url: TRPC_URL,
+        transformer: SuperJSON,
         headers: () => {
           const cookie = request?.headers.get("Cookie") || "";
           return { cookie };
@@ -20,11 +20,11 @@ export const trpcServer = (request?: Request) => {
 };
 
 // use trpcClient to fetch in frontend environment, like in react component, cookies will be passing to trpc endpoint automatically
-export const trpcClient = createTRPCProxyClient<AppRouter>({
-  transformer: SuperJSON,
+export const trpcClient = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
       url: TRPC_URL,
+      transformer: SuperJSON,
     }),
   ],
 });
